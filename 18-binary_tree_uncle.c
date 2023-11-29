79b7fac25f147a2d48864d9b8c93ef1942c065f0
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-11-29 13:56:01 +0200

    Update

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - Finds the uncle of a node
+ * binary_tree_uncle - Locates the uncle of a node
  *                     in a binary tree.
  * @node: A pointer to the node to find the uncle of.
  *
