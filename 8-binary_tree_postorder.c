b8e0187e2e4f9df5a288d7e2db969b2597aae17d
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-11-29 14:03:56 +0200

    Update

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - Covers a binary tree using post-order traversal.
- * @tree: A pointer to the root node of the tree to traverse.
+ * binary_tree_postorder - Covers a binary tree using post-order courses.
+ * @tree: A pointer to the root node of the tree to cover.
  * @func: A pointer to a function to call for each node.
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
