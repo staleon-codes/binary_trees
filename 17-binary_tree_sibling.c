f9f9ca7bf04c3c8471d5bbe4efa79e4aa2b114c5
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-11-29 13:55:07 +0200

    Update

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - Finds the sibling of a
+ * binary_tree_sibling - Spots the sibling of a
  *                       node in a binary tree.
  * @node: A pointer to the node to find the sibling of.
  *
