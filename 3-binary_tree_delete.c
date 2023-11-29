c1fe8a071e3296b0877d4f075827608aa11ebba7
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-11-29 13:58:03 +0200

    Update

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - Deletes a binary tree.
+ * binary_tree_delete - Removes a binary tree.
  * @tree: A pointer to the root node of the tree to delete.
  */
 void binary_tree_delete(binary_tree_t *tree)
